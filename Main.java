95c58b27d0c9a5062bb1e1281065384ecf9b9c28
bwhitmore7 <bryson.whitmore@patelhs.org>
2021-01-07 15:32:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Student obj1 = new Student(4.1, 260.0, "henry", 6, true);
+    Student obj2 = new Student(8.7, 16.0, "clarence", 2, true);
+obj1.rollerCoaster();
+obj2.rollerCoaster();
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("what is your height?");
+    double height = scan.nextDouble();
+    System.out.println("how much do you weigh?");
+    double weight = scan.nextDouble();
+    System.out.println("what is your name?");
+    String name = scan.next();
+    System.out.println("how old are you?");
+    int age = scan.nextInt();
+    System.out.println("is it sunny outside? true/false");
+    boolean weather = scan.nextBoolean();
+
+    Student obj3 = new Student(height, weight, name, age, weather);
+    obj3.rollerCoaster();
+  }
+}
---
+++ Student.java
@@ -0,0 +1,28 @@
+public class Student{
+//variables
+private double height;
+private double weight;
+private String name;
+private int age;
+private boolean goodWeather;
+
+//constructor
+public Student(double studentHeight, double studentWeight, String studentName, int studentAge, boolean Weather)
+{
+  height = studentHeight;
+  weight = studentWeight;
+  name = studentName;
+  age = studentAge;
+  goodWeather = Weather;
+}
+//method
+public void rollerCoaster()
+ {
+  if(height < 4.6 && weight < 300.0 && age > 5 && goodWeather)
+  {
+    System.out.println("yay! you get to ride the roller coaster" + name);
+  }else{
+    System.out.println("sorry, maybe next year" + name);
+  }
+ }
+}
